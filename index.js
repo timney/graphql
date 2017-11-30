c984a4d695b3451ca97f8baf3d8e6b07d8d73bed
Andrew Timney <andrew.timney@gmail.com>
2017-11-30 16:24:19 +0000

    Tidy up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 const graphqlHTTP = require('express-graphql');
+
 var schema = require('./schema');
 const userData = require('./MOCK_DATA.json');
 
@@ -12,26 +13,16 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/graphql', graphqlHTTP({
+const graphqlConfig = {
     schema: schema,
     graphiql: true,
     context: {
         userData: userData
     }
-  }));
+};
 
-  server.post('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: false,
-    context: {
-        userData: userData
-    }
-  }));
-
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
+server.get('/graphql', graphqlHTTP(graphqlConfig));
+server.post('/graphql', graphqlHTTP(graphqlConfig));
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
